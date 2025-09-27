5202b1845bc6dca7d989aecca91d93aa5f15d901
Ehabw57 <maestro.ehab123@gmail.com>
2025-09-27 16:35:39 +0300

    Add user model

---
+++ backend/models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "student", "instructor", "admin", "super_admin"],
+      default: "user",
+    },
+    profile_pic: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(8);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (targetPassword) {
+  return bcrypt.compare(targetPassword, this.password);
+};
+
+UserSchema.index({ email: 1 });
+
+module.exports = mongoose.model("User", UserSchema);
