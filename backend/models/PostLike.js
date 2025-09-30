78b470bdb6e641a39f35755bfc657dd50a1596f4
ghadasalah26 <ghadasalah448@gmail.com>
2025-09-30 13:37:56 +0300

    Add PostLike file

---
+++ backend/models/PostLike.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const postLikeSchema = new mongoose.Schema({
+  post_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",   
+    required: true,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",  
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+postLikeSchema.index(
+  { post_id: 1, user_id: 1 },
+  { unique: true }
+);
+
+const PostLike = mongoose.model("PostLike", postLikeSchema);
+
+module.exports = PostLike;
