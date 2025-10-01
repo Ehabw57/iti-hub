c45441f86e499e5a2215a42c93ef98e9849e0f45
ghadasalah26 <ghadasalah448@gmail.com>
2025-10-01 18:48:22 +0300

    Final edit after testing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,18 @@
-const express = require ("express")
-const {getAllUsers , getUserById, updateUser, deleteUser, createUser}=require("../controllers/userController")
+const express = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  createUser,
+} = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/user", createUser);
 userRouter.get("/user", getAllUsers);
-userRouter.get("/users/:id ", getUserById);
-userRouter.put("/users/:id ", updateUser);
-userRouter.delete("/users/:id ", deleteUser);
+userRouter.get("/users/:id", getUserById);
+userRouter.put("/users/:id", updateUser);
+userRouter.delete("/users/:id", deleteUser);
 
-
-module.exports =  userRouter
+module.exports = userRouter;
