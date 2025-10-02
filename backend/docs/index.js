611d498064aebaf5ed0cb3fadc0269f637368f0e
Your Name <ayatkady9@gmail.com>
2025-10-02 14:53:14 +0300

    added swagger

--- backend/docs/index.js
+++ backend/docs/index.js
@@ -1,6 +1,9 @@
 const YAML = require("yamljs");
 
 const commentDoc = YAML.load("./docs/comment.yaml");
+const userDoc = YAML.load("./docs/user.yaml");
+
+
 
 const base = {
   openapi: "3.0.0",
@@ -19,11 +22,14 @@ const swaggerDocument = {
   ...base,
   paths: {
     ...base.paths,
-    ...commentDoc.paths
+    ...commentDoc.paths,
+    ...userDoc.paths
+
   },
   components: {
     ...base.components,
-    ...commentDoc.components
+    ...commentDoc.components,
+    ...userDoc.components
   },
 };
 module.exports = swaggerDocument;
