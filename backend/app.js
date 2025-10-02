e4e39b4919ca96c45179f003ac7f909674925649
Ehab Hegazy <136044546+Ehabw57@users.noreply.github.com>
2025-10-02 14:04:35 +0300

    Add swagger docs for comments
    * Edit comments CRUD oprations

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors')
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("./docs/index")
 
 const commentRoute = require('./routes/commentRoutes');
 const messageRoute = require('./routes/messageRoutes');
@@ -13,9 +16,11 @@ const PORT = process.env.PORT || 3030;
 const DBURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/iti-hub";
 
 app.use(express.json());
+app.use(cors())
 app.use(commentRoute);
 app.use(messageRoute);
 app.use(userRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose
   .connect(DBURL)
@@ -23,9 +28,12 @@ mongoose
     console.log("Connected to DB");
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Docs at http://localhost:${PORT}/api-docs`)
     });
   })
   .catch((err) => {
     console.error(err.message);
     process.exit(1);
   });
+
+module.exports = PORT
