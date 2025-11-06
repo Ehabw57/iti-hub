e5119792a1fbba9bf7682e3ac8228def5994960d
Ehabw57 <maestro.ehab123@gmail.com>
2025-11-06 09:41:23 +0200

    Handel connection accept/block endpoints

--- backend/routes/connectionRoutes.js
+++ backend/routes/connectionRoutes.js
@@ -5,7 +5,8 @@ const {
   getReceivedRequests,
   getSentRequests,
   sendConnectionRequest,
-  getConnections
+  getConnections,
+  handleConnection
 } = require("../controllers/connectionController");
 
 connectionRoute.delete("/connections/:id", deleteConnection);
@@ -13,5 +14,7 @@ connectionRoute.post("/connections/request/:id", sendConnectionRequest);
 connectionRoute.get("/connections/requests", getReceivedRequests);
 connectionRoute.get("/connections/requests/sent", getSentRequests);
 connectionRoute.get("/connections", getConnections);
+connectionRoute.put("/connections/:id/accept", (req, res) => handleConnection(req, res, "accept"));
+connectionRoute.put("/connections/:id/block", (req, res) => handleConnection(req, res, "block"));
 
 module.exports = connectionRoute;
